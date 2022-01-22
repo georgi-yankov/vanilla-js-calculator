052f3493cf51d89a8ad726dcc34030501f4db4b8
Georgi Yankov <georgi.st.yankov@gmail.com>
2022-01-22 14:33:47 +0200

    add video tutorial link

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Video tutorial:
+// https://youtu.be/j59qQ7YWLxw?list=PLZlA0Gpn_vH9k5ju1yq9qCDqvtuTVgTr6
+
 class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement
